5075983b2200c1274e38aea36ba7eeeb32d36a92
Mark Padgett <mark@sluggerdesign.com>
2016-01-09 16:24:47 -0500

    Fixed Authentication Error
    Removed disabled field from authentication

--- application/libraries/authentication.php
+++ application/libraries/authentication.php
@@ -10,14 +10,14 @@
 
 	// Try Login
 	function try_login($condition = array()) {
-		$this->CI->db->select('id, name, email, status');
+		$this->CI->db->select('id, name, email');
 		$query = $this->CI->db->get_where('users', $condition, 1, 0);
 
 		if ($query->num_rows() != 1) {
 			return FALSE;
 		} else {
 			$row = $query->row();
-			if($row->status == 'Disabled') return FALSE;
+			// if($row->status == 'Disabled') return FALSE;
 			$this->CI->session->set_userdata(array('id'=>$row->id, 'email'=>$row->email, 'name'=>$row->name));
 			return TRUE;
 		}
